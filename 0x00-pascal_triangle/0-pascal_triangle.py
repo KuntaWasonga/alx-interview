17b8520581f8d429f9bc0fb14bb74251e6f0a9f4
KuntaWasonga <wkunta@rocketmail.com>
2023-03-27 16:22:47 +0000

    Corrected pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,8 +14,8 @@ def pascal_triangle(n):
     prev_row = []
     if (n > 0):
         for i in range(0, n + 1):
-            row = [j > 0 and j < i - 1 and i > 2 and prev_row[j-1]
-             + prev_row[j] or 1 for j in range(0, i)]
+            row = [j > 0 and j < i - 1 and i > 2 and
+             prev_row[j-1] + prev_row[j] or 1 for j in range(0, i)]
             prev_row = row
             triangle += [row]
         return triangle[1:]
