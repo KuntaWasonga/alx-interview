77418bcd767c40d5d222c3bf2e061c3c1af167e7
KuntaWasonga <wkunta@rocketmail.com>
2023-05-29 14:05:12 +0300

    Updated README

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module to compute island perimeter.
+"""
+
+def island_perimeter (grid):
+    """Function that returns perimeter of the island
+    described in grid.
+    """
+    if type(grid) != list:
+        return 0
+
+    perimeter = 0
+    l = len(grid)
+
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == l - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+
+    return perimeter
