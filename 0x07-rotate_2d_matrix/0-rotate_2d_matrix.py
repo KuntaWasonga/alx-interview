81e3ab9d81eaf42538be1431f605d30ad349e2a3
KuntaWasonga <wkunta@rocketmail.com>
2023-07-05 13:35:36 +0300

    Correct pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,16 +2,17 @@
 """ This module is Task 7 of the alx interview
 """
 
+
 def rotate_2d_matrix(matrix):
     """ This function rotates a 2D matrix 90 degrees clockwise
     """
     n = len(matrix)
 
-    #transpose matrix
+    # transpose matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    #reverse rows in matrix
+    # reverse rows in matrix
     for i in range(n):
         matrix[i] = matrix[i][::-1]
