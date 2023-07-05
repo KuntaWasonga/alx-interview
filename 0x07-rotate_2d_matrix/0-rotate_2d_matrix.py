63a61a19d53020e480eb3cdb922d88185859432e
KuntaWasonga <wkunta@rocketmail.com>
2023-07-05 13:30:14 +0300

    Task 0

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This module is Task 7 of the alx interview
+"""
+
+def rotate_2d_matrix(matrix):
+    """ This function rotates a 2D matrix 90 degrees clockwise
+    """
+    n = len(matrix)
+
+    #transpose matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    #reverse rows in matrix
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
